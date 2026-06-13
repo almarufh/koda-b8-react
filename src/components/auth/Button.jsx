9950c15b027badd25dfdd6e6245e07490ab0b6fb
almarufh <hidayatmaruf99@gmail.com>
2026-06-13 13:01:22 +0800

    feat: create new componen Button

---
+++ src/components/auth/Button.jsx
@@ -0,0 +1,21 @@
+export default function Button(props) {
+    const order = {
+        "1": "order-1",
+        "none": "order-none"
+    };
+
+    return (
+        <button 
+            type="submit" 
+            style={{ backgroundColor: props.color }}
+            className="flex items-center justify-center gap-2 p-[14px] rounded-[12px]"
+        >
+            <img 
+                src={props.src} 
+                className={order[props.order]}
+                alt={props.action}
+            />
+            <span className="text-white text-[16px]">{props.action}</span>
+        </button>
+    );
+}
