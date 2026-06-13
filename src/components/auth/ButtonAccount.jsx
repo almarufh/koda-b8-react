1613979a17ff4fb497f2a1e74bb5dd68b77c80a4
almarufh <hidayatmaruf99@gmail.com>
2026-06-13 10:33:49 +0800

    feat: create new componen Header

---
+++ src/components/auth/ButtonAccount.jsx
@@ -0,0 +1,7 @@
+function ButtonAccount (props) {
+    return (
+        <button type="button" className="w-full border border-[#0000001A] rounded-[12px] py-[12px]">{props.result}</button>
+    )
+}
+
+export default ButtonAccount
