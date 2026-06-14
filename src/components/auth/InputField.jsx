6570227526dd912f655908f01aa5a7d22c8ce430
almarufh <hidayatmaruf99@gmail.com>
2026-06-14 12:11:32 +0800

    fix: componen for opstion view Lupa kata sandi

--- src/components/auth/InputField.jsx
+++ src/components/auth/InputField.jsx
@@ -1,9 +1,21 @@
+import { Link } from 'react-router';
+
 function InputField(props) {
   return (
     <div className="flex flex-col gap-1">
-        <label htmlFor={props.name} className="text-[14px] text-[#111827]">
-            {props.label}
-        </label>
+        <div className="grid grid-cols-2 text-[14px] text-[#111827] justify-between items-center">
+            <label htmlFor={props.name} className="text-[14px] text-[#111827]">
+                {props.label}
+            </label>
+            {props.show && (
+                <Link 
+                    className='text-[#1A73E8] justify-self-end' 
+                    to="/auth/forgot-password"
+                >
+                    Lupa kata sandi?
+                </Link>
+            )}
+        </div>
         <div 
             className="flex items-center gap-3 border border-[#0000001A] bg-[#F3F4F6] py-[13.5px] px-[16px] rounded-[12px]">
             <img 
