b2dd5f96d3830ba730322c1e7bdc8d55ac653248
almarufh <hidayatmaruf99@gmail.com>
2026-06-19 06:56:37 +0800

    fix: change structur path try use children

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,42 @@ import { RouterProvider, createBrowserRouter } from 'react-router';
 import Login from './pages/auth/Login.jsx';
 import Register from './pages/auth/Register.jsx';
 import Forgot from './pages/auth/Forgot.jsx';
-import LandingPage from './pages/main/LandingPage.jsx';
+// import LandingPage from './pages/main/LandingPage.jsx';
+import React from 'react';
+import AuthContext from './contexts/AuthContext.jsx';
+import AuthLayout from './pages/auth/AuthLayout.jsx';
 
 const router = createBrowserRouter([
+  // {
+  //   path: '/',
+  //   element: <LandingPage />,
+  // },
   {
-    path: '/',
-    element: <LandingPage />,
-  },
-  {
-    path: '/auth/login',
-    element: <Login />,
-  },
-  {
-    path: '/auth/register',
-    element: <Register />,
-  },
-  {
-    path: '/auth/forgot-password',
-    element: <Forgot />,
+    path: '/auth',
+    element:  <AuthLayout />,
+    children: [
+      {
+        path: '/auth/login',
+        element: <Login />,
+      },
+      {
+        path: '/auth/register',
+        element: <Register />,
+      },
+      {
+        path: '/auth/forgot-password',
+        element: <Forgot />,
+      }
+    ]
   },
 ])
 
 function App() {
+  const [auth, setAuth] = React.useState(AuthContext)
   return (
-    <RouterProvider router={router} />
+    <AuthContext value={{auth, seAuth}}>
+      <RouterProvider router={router} />
+    </AuthContext>
   );
 }
 
