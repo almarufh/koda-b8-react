9d1f3caf73f6b72a27c62acfe4973d4ac5b4f987
almarufh <hidayatmaruf99@gmail.com>
2026-06-19 15:57:19 +0800

    fix: deleted Header and footer because have in main layout

--- src/pages/main/LandingPage.jsx
+++ src/pages/main/LandingPage.jsx
@@ -1,12 +1,11 @@
-import Footer from "../../components/main/Footer";
-import Header from "../../components/main/Header";
-
 export default function LandingPage () {
-
+    
     return (
-    <>
-        <Header/>
-        <Footer/>
-    </>
+        <div className="flex flex-col items-center bg-red-500 w-full max-w-[1728px] h-screen">
+            <div className="grid grid-cols-2 w-full h-105">
+                <div className="border w-full h-105"></div>
+                <div className="border w-full h-105"></div>
+            </div>
+        </div>
     )
 }
