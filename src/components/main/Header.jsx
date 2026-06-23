912d53bf681aa9db9118fc80e1bc1f3e43c62bfd
almarufh <hidayatmaruf99@gmail.com>
2026-06-23 22:30:37 +0800

    fix: delete satauan constnt used persend and responsive

--- src/components/main/Header.jsx
+++ src/components/main/Header.jsx
@@ -1,6 +1,6 @@
 import Logo from '../auth/Logo'
 import { Image } from '../../assets/index.js'
-import { Outlet, useNavigate } from 'react-router'
+import { useNavigate } from 'react-router'
 import React from 'react';
 import { LuLogOut } from "react-icons/lu";
 
@@ -31,21 +31,21 @@ export default function Header() {
 
     function Navimg (props) {
         const color = {
-            default: "text-[14px] text-[#6B7280]",
-            promo: "text-[14px] text-[#DC2626]"
+            default: "text-[12px] xl:text-[14px] text-[#6B7280]",
+            promo: "text-[12px] xl:text-[14px] text-[#DC2626]"
         }
         return (
-            <div className="flex items-center gap-1.5 px-4 py-2.5 w-fit ">
-                <span className="text-[14px] text-[#6B7280]">{props.img}</span>
+            <div className="flex items-center gap-1.5 p-2 xl:px-4 py-2.5 w-fit ">
+                <span className="text-[12px] xl:text-[14px] text-[#6B7280]">{props.img}</span>
                 <span className={color[props.style] || color.default}>{props.text}</span>
             </div>
         )
     }
 
   return (
-    <div className='border flex flex-col w-full mx-auto items-center'>
-        <div className="flex items-center justify-center w-full max-w-432 bg-[#1A73E8]">
-            <div className='flex items-center justify-between w-full max-w-7xl pl-4 pr-4'>
+    <div className='sticky t-0 flex flex-col w-full items-center'>
+        <div className="flex items-center justify-center w-full hidden md:flex bg-[#1A73E8]">
+            <div className='flex items-center justify-between w-full md:w-[95%] xl:w-[90%] 2xl:w-[80%] pl-4 pr-4'>
                 <div className='flex items-center gap-1'>
                     <img
                     src="/assets/main/landingpage/map.svg"
@@ -61,10 +61,10 @@ export default function Header() {
             </div>
 
         </div>
-        <div className="flex items-center justify-center w-full max-w-432 bg-[#FFFFFF01]">
-            <div className="flex flex-col md:flex-row items-center justify-start w-full max-w-7xl pl-4 py-1 gap-4">
+        <div className="flex items-center justify-center w-full bg-[#FFFFFF01]">
+            <div className="grid grid-cols-2 md:grid-cols-[auto_1fr_auto] items-center justify-start w-full md:w-[95%] xl:w-[90%] 2xl:w-[80%] pl-4 py-1 gap-4 xl:gap-10">
                 <Logo style="#1A73E8" text="black"/>
-                <div className="flex items-center w-full border border-[#0000001A] rounded-md">
+                <div className="order-3 md:order-0 col-span-2 md:col-span-1 flex items-center w-full border border-[#0000001A] rounded-md">
                     <input 
                         type="text" 
                         className='flex px-3 outline-none w-full text-[12px]'
@@ -78,11 +78,11 @@ export default function Header() {
                         />
                     </button>
                 </div>
-                <div className="flex gap-1 w-full">
+                <div className="order-2 md:order-0 justify-end flex gap-1 w-full">
                     <img 
                         src="/assets/main/landingpage/bell.svg" 
                         alt="Bell" 
-                        className='p-[10px]'
+                        className='p-2.5'
                     />
                     <div className="flex items-center gap-3">
                         <div 
@@ -117,11 +117,11 @@ export default function Header() {
                 </div>
             </div>
         </div>
-        <div className="border border-[#0000001A] border-x-0 flex items-center justify-center w-full max-w-432 bg-[#FFFFFF01]">
-            <div className="flex  flex-col md:flex-row items-start md:items-center justify-start w-full max-w-7xl p-1">
-                <div className="flex items-center  px-4 py-2.5 gap-1">
+        <div className="border border-[#0000001A] border-x-0 flex items-center justify-start md:items-center w-full bg-[#FFFFFF01]">
+            <div className="flex flex-col md:flex-row items-start md:items-center justify-start w-fit md:w-full md:w-[95%] xl:w-[90%] 2xl:w-[80%] p-1">
+                <div className="flex items-center px-2 xl:px-4 py-2.5 gap-1">
                     <img src="/assets/main/landingpage/menu.svg" alt="Menu" />
-                    <span className="text-[14px] text-[#111827]">Semua Kategori</span>
+                    <span className="text-[12px] xl:text-[14px] text-[#111827]">Semua Kategori</span>
                     <Image.Down className="self-center size-[14px]"/>
                 </div>
                 <Navimg img="💻" text="Elektronik" />
@@ -133,9 +133,6 @@ export default function Header() {
                 <Navimg img="🔥" text="Promo" style="promo" />
             </div>
         </div>
-        <main className='flex flex-col items-center w-full'>
-            <Outlet />
-        </main>
     </div>
   )
 }
