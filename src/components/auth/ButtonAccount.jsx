980f5b80adc8ffae2d914648ae03d442ce40d882
almarufh <hidayatmaruf99@gmail.com>
2026-06-25 11:37:07 +0800

    delete: delet old for backup

--- src/components/auth/ButtonAccount.jsx
+++
@@ -1,7 +0,0 @@
-function ButtonAccount (props) {
-    return (
-        <button type="button" className="w-full border border-[#0000001A] rounded-[12px] py-[12px]">{props.result}</button>
-    )
-}
-
-export default ButtonAccount
