7b014189da5b38af3f6510d14b672e1d39681914
almarufh <hidayatmaruf99@gmail.com>
2026-06-25 11:47:41 +0800

    feat: create new folder for learning react for can write projrct

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { createBrowserRouter } from 'react-router'
+
+const router = createBrowserRouter([
+    {
+        path: 'auth';
+        element: 
+    }
+])
+
+function App() {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
