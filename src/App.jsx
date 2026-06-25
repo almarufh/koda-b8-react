89678fed193ff7069871762f9ca9480e9a729065
almarufh <hidayatmaruf99@gmail.com>
2026-06-25 14:03:57 +0800

    feat: add structur path link

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
 import React from 'react'
-import { createBrowserRouter } from 'react-router'
-import AuthLayout from '../src old/pages/auth/AuthLayout'
-import Login from '../src old/pages/auth/Login'
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router'
+import AuthLayout from '../src/pages/auth/AuthLayout'
+import Login from '../src/pages/auth/Login'
 import ForgotPaasword from './pages/auth/ForgotPaasword'
-import Register from '../src old/pages/auth/Register'
+import Register from '../src/pages/auth/Register'
+import MainLayout from './pages/main/MainLayout'
+import LandingPage from './pages/main/LandingPage'
+import BrowseProducts from './pages/main/toko/BrowseProducts'
+import DetailPage from './pages/main/toko/DetailPage'
+import Cart from './pages/main/Cart'
+import AddresList from './pages/main/profile/AddresList'
+import TokoLayout from './pages/main/toko/TokoLayout'
+import ProfileLayout from './pages/main/profile/ProfileLayout'
+import MyOrder from './pages/main/profile/MyOrder'
+import Wishlist from './pages/main/profile/Wishlist'
+import EditProfile from './pages/main/profile/EditProfile'
+import CheckoutLayout from './pages/main/checkout/CheckoutLayout'
+import StepFirst from './pages/main/checkout/StepFirst'
+import StepSecond from './pages/main/checkout/StepSecond'
+import StepThird from './pages/main/checkout/StepThird'
 
 const router = createBrowserRouter([
     {
         path: '/auth',
         element: <AuthLayout />,
         children: [
+            // {
+            //     index: true,
+            //     element: <Navigate to='login' replace />
+            // },
             {
                 path: 'login',
                 element: <Login />
@@ -23,12 +42,84 @@ const router = createBrowserRouter([
                 element: <Register />
             }
         ]
+    },
+    {
+        path: '/main',
+        element: <MainLayout />,
+        children: [
+            {
+                index: true,
+                element: <Navigate to='home' replace />
+            },
+            {
+                path: 'home',
+                element: <LandingPage />
+            },
+            {
+                path: '',
+                element: <TokoLayout />,
+                children: [
+                    {
+                        path: 'toko',
+                        element: <BrowseProducts />
+                    },
+                    {
+                        path: 'products',
+                        element: <DetailPage />
+                    }
+                ]
+            },
+            {
+                path: 'cart',
+                element: <Cart />
+            },
+            {
+                path: 'profile',
+                element: <ProfileLayout />,
+                children: [
+                    {
+                        path: 'order',
+                        element: <MyOrder />
+                    },
+                    {
+                        path: 'whislist',
+                        element: <Wishlist />
+                    },
+                    {
+                        path: 'addres',
+                        element: <AddresList />
+                    },
+                    {
+                        path: 'edit',
+                        element: <EditProfile />
+                    }
+                ]
+            },
+            {
+                path: 'checkout',
+                element: <CheckoutLayout />,
+                children: [
+                    {
+                        path: 'step-first',
+                        element: <StepFirst />
+                    },
+                    {
+                        path: 'step-second',
+                        element: <StepSecond />
+                    },
+                    {
+                        path: 'step-third',
+                        element: <StepThird />
+                    }
+                ]
+            }
+        ]
     }
 ])
 
 function App() {
   return (
-    <div>App</div>
+    <RouterProvider router={router}/> 
   )
 }
 
